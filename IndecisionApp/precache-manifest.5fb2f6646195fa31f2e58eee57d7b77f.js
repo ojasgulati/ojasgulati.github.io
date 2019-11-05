a4d601840403eadb89af171cd140500b25a06bad
Ojas Gulati <ojasgulati100@gmail.com>
2019-11-05 07:24:23 +0530

    Indecision App Added

---
+++ IndecisionApp/precache-manifest.5fb2f6646195fa31f2e58eee57d7b77f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b125cf964e4bec3864127eb98c9c0644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad2598be39eb5d85d17",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3e3080ce2dc248f6ee75",
+    "url": "/static/css/main.7c28b704.chunk.css"
+  },
+  {
+    "revision": "9ad2598be39eb5d85d17",
+    "url": "/static/js/2.0f3ff764.chunk.js"
+  },
+  {
+    "revision": "3e3080ce2dc248f6ee75",
+    "url": "/static/js/main.97307c1a.chunk.js"
+  },
+  {
+    "revision": "31ef752cde4df14b7442",
+    "url": "/static/js/runtime-main.86c0c4c8.js"
+  }
+]);
