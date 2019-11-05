80219181e198aa9da7f7959a88dcbaaa1c7634ad
Ojas Gulati <ojasgulati100@gmail.com>
2019-11-05 07:45:04 +0530

    Updates

---
+++ precache-manifest.6488d1d42d3d0d84613a97f749e0a180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b05b4fa56cc4efdb9dce951b2616ba",
+    "url": "/IndecisionApp/index.html"
+  },
+  {
+    "revision": "9ad2598be39eb5d85d17",
+    "url": "/IndecisionApp/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "22a1865f1e289b973708",
+    "url": "/IndecisionApp/static/css/main.7c28b704.chunk.css"
+  },
+  {
+    "revision": "9ad2598be39eb5d85d17",
+    "url": "/IndecisionApp/static/js/2.0f3ff764.chunk.js"
+  },
+  {
+    "revision": "22a1865f1e289b973708",
+    "url": "/IndecisionApp/static/js/main.351fe7c0.chunk.js"
+  },
+  {
+    "revision": "f99e0375132c30d8c36d",
+    "url": "/IndecisionApp/static/js/runtime-main.cef47cfc.js"
+  }
+]);
